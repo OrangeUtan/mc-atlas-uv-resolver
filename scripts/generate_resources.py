0289c8722113a7cf802915980a8a3f7760574e42
Oran9eUtan <Oran9eUtan@gmail.com>
2021-06-27 14:36:57 +0200

    chore: Generating atlases resource

--- scripts/generate_resources.py
+++ scripts/generate_resources.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from zipfile import ZipFile
 
@@ -12,12 +13,27 @@ out_root = Path("dist").absolute()
 
 @app.command()
 def cmd_main(minecraft_version: str):
+    generate_uv_resources(minecraft_version)
+    generate_atlas_resources(minecraft_version)
+
+
+def generate_uv_resources(minecraft_version: str):
     for category in get_uv_categories(minecraft_version):
         archive_path = out_root / minecraft_version / f"uvs_{category}.zip"
         archive_path.parent.mkdir(parents=True, exist_ok=True)
         with ZipFile(archive_path, "w") as archive:
             for path in (settings.uvs_root / minecraft_version / category).iterdir():
                 archive.write(path, path.name)
+
+
+def generate_atlas_resources(minecraft_version: str):
+    archive_path = out_root / minecraft_version / "atlases.zip"
+    archive_path.parent.mkdir(parents=True, exist_ok=True)
+    with ZipFile(archive_path, "w") as archive:
+        for folder, _, filenames in os.walk(settings.atlases_root / minecraft_version):
+            for file in filenames:
+                path = Path(folder, file)
+                archive.write(path, path.relative_to(settings.atlases_root / minecraft_version))
 
 
 def get_uv_categories(minecraft_version: str):
