91fbc6f9dcc7dc5aee591ff35e9cd0dcbe711825
Oran9eUtan <Oran9eUtan@gmail.com>
2021-06-27 15:21:42 +0200

    chore: Combined uvs into 1 resource

--- scripts/generate_resources.py
+++ scripts/generate_resources.py
@@ -18,26 +18,28 @@ def cmd_main(minecraft_version: str):
 
 
 def generate_uv_resources(minecraft_version: str):
-    for category in get_uv_categories(minecraft_version):
-        archive_path = out_root / minecraft_version / f"uvs_{category}.zip"
-        archive_path.parent.mkdir(parents=True, exist_ok=True)
-        with ZipFile(archive_path, "w") as archive:
-            for path in (settings.uvs_root / minecraft_version / category).iterdir():
-                archive.write(path, path.name)
+    archive_path = out_root / minecraft_version / "uvs.zip"
+    archive_path.parent.mkdir(parents=True, exist_ok=True)
+    with ZipFile(archive_path, "w") as archive:
+        add_dir_to_archive(archive, settings.uvs_root / minecraft_version)
 
 
 def generate_atlas_resources(minecraft_version: str):
     archive_path = out_root / minecraft_version / "atlases.zip"
     archive_path.parent.mkdir(parents=True, exist_ok=True)
     with ZipFile(archive_path, "w") as archive:
-        for folder, _, filenames in os.walk(settings.atlases_root / minecraft_version):
-            for file in filenames:
-                path = Path(folder, file)
-                archive.write(path, path.relative_to(settings.atlases_root / minecraft_version))
+        add_dir_to_archive(archive, settings.atlases_root / minecraft_version)
 
 
 def get_uv_categories(minecraft_version: str):
     return map(lambda a: a.name, (settings.uvs_root / minecraft_version).iterdir())
+
+
+def add_dir_to_archive(archive: ZipFile, dir: Path):
+    for folder, _, filenames in os.walk(dir):
+        for file in filenames:
+            path = Path(folder, file)
+            archive.write(path, path.relative_to(dir))
 
 
 settings = Settings.load(Path("scripts/settings.yml"))
