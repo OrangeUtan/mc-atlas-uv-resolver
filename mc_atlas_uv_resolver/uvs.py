f1449574e57ac795eec0bb17e0bff80b8e2cfa8d
Oran9eUtan <Oran9eUtan@gmail.com>
2021-07-10 12:56:45 +0200

    fix: Images not read with transparency

--- mc_atlas_uv_resolver/uvs.py
+++ mc_atlas_uv_resolver/uvs.py
@@ -1,19 +1,24 @@
 from pathlib import Path
 
 import cv2
-from PIL.Image import Image
+from numpy import ndarray
 
 __all__ = ["find_texture_uvs_in_atlas", "find_all_texture_uvs_in_atlas"]
 
 
-def find_texture_uvs_in_atlas(texture: Image, atlas: Image) -> tuple[int, int]:
+def find_texture_uvs_in_atlas(texture: ndarray, atlas: ndarray) -> tuple[int, int]:
     _, _, location, _ = cv2.minMaxLoc(cv2.matchTemplate(atlas, texture, cv2.TM_SQDIFF_NORMED))
     return location
 
 
 def find_all_texture_uvs_in_atlas(textures: list[Path], atlas_path: Path):
-    atlas = cv2.imread(str(atlas_path))
+    atlas: ndarray = cv2.imread(str(atlas_path), cv2.IMREAD_UNCHANGED)
     for path in textures:
-        texture: Image = cv2.imread(str(path))
-        u, v = find_texture_uvs_in_atlas(atlas, texture)
+        texture: ndarray = cv2.imread(str(path), cv2.IMREAD_UNCHANGED)
+
+        # Add alpha channel if missing
+        if texture.shape[2] != 4:
+            texture = cv2.cvtColor(texture, cv2.COLOR_RGB2RGBA)
+
+        u, v = find_texture_uvs_in_atlas(texture, atlas)
         yield path, u, v
